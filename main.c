9b8fc31f6dc7ddf832a6489dd75f63452d08065c
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-10-11 18:00:28 -0600

    Add files via upload

---
+++ main.c
@@ -0,0 +1,41 @@
+/*
+    b2{
+        calcular la media de estos
+    }
+    
+    b3,b4{
+        ordenamiento burbuja
+    }
+
+    que me diga cual es el menor y cual el mayor
+*/
+#include<stdio.h>
+
+int main(){    
+    int nums[100];
+    int n;
+    int n2;
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    int l = 0;
+    float promedio;
+
+    printf("dime cuantos datos quieres ingresar, no mas de 100: ");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("ingresa un dato: ");
+        scanf("%d", &n2);
+        nums[i] = n2;
+    }
+
+    i++;
+
+    for(j = 0 ; j < i ; j++){
+        
+    }    
+
+    return 0;
+}
