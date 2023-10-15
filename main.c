ad980284b00d030fc9162d3158a55c365067217d
Antonio-Zabdiel <112569142+Antonio-Zabdiel@users.noreply.github.com>
2023-10-15 12:02:56 -0600

    promedio

--- main.c
+++ main.c
@@ -1,8 +1,4 @@
 /*
-    b2{
-        calcular la media de estos
-    }
-    
     b3,b4{
         ordenamiento burbuja
     }
@@ -13,29 +9,33 @@
 
 int main(){    
     int nums[100];
-    int n;
-    int n2;
-    int i = 0;
-    int j = 0;
+    int n;//
+    int n2;//
+    int i = 0;//#1
+    int j = 0;//#2
     int k = 0;
     int l = 0;
-    float promedio;
+    float promedio = 0.0;//
 
     printf("dime cuantos datos quieres ingresar, no mas de 100: ");
     scanf("%d", &n);
 
-    for (i = 0; i < n; i++)
-    {
+    //bucle uno, registro de datos #1
+    for (i = 0; i < n; i++){
         printf("ingresa un dato: ");
         scanf("%d", &n2);
         nums[i] = n2;
     }
 
-    i++;
-
+    //bucle 2 para la suma usada en el promecsio #2
+    //seccion de promedio
     for(j = 0 ; j < i ; j++){
-        
-    }    
+        promedio += nums[j];
+        printf("%f\n", promedio);  
+    }  
+
+    promedio /= i;
+     
 
     return 0;
 }
